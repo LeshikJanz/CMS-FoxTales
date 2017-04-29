3a216d2fa5c045dfdc02915b17f0707c19eb72c2
Andrei Katalkin <andrei.katalkin@gmail.com>
2017-04-29 21:47:33 +0300

    Remove HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { APP_RESOLVER_PROVIDERS } from './app.resolver';
 import { AppState, InternalStateType } from './app.service';
-import { HomeComponent } from './home';
 import { NoContentComponent } from './no-content';
 
 import '../styles/styles.scss';
@@ -38,7 +37,6 @@ type StoreType = {
   bootstrap: [ AppComponent ],
   declarations: [
     AppComponent,
-    HomeComponent,
     NoContentComponent
   ],
   imports: [ // import Angular's modules
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home';
 import { NoContentComponent } from './no-content';
 
 export const ROUTES: Routes = [
-  { path: '',      component: HomeComponent },
-  { path: 'home',  component: HomeComponent },
   { path: '',      loadChildren: 'app/user/user.module' },
   { path: '**',    component: NoContentComponent },
 ];
