c1d177104e0521687863d1ddca50d8b9f6dd0569
Matthew Lee <matthew@getfoxtales.com>
2017-05-15 14:33:38 -0700

    updated routes to add events

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const ROUTES: Routes = [
 
   { path: '', loadChildren: 'app/auth/auth.module#AuthModule' },
   { path: 'admin', loadChildren: 'app/admin/admin.module#AdminModule', canActivate: [ AuthGuard ] },
+  { path: 'events', loadChildren: 'app/events/events-container.module#EventsModule', canActivate: [ AuthGuard ] },
   { path: '**',    component: NoContentComponent }
 
 ];
