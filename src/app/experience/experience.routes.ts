2715f72b0b8e999df6e803dac91bcd6a51025591
Matthew Lee <matthew@getfoxtales.com>
2017-05-15 15:01:47 -0700

    updated routes so experience will work

--- src/app/experience/experience.routes.ts
+++ src/app/experience/experience.routes.ts
@@ -4,6 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExperienceListComponent } from './list';
 
 const EXPERIENCE_ROUTES: Routes = [
+  { path: '',    redirectTo: 'experience' },
   { path: 'experience/:id',    component: ExperienceListComponent },
 ];
 
