f8bc0662891182ea798906604f7f2027981a061e
Matthew Lee <matthew@getfoxtales.com>
2017-05-18 09:41:35 -0700

    changed file name to component for ui builder and finished majority of it, just need to hook up backend enpoints to save

---
+++ src/app/experience-builder/ui-builder/ui-builder.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ColorPickerService, Rgba } from 'ngx-color-picker';
+import { TabsetComponent } from 'ngx-bootstrap';
+
+@Component({
+  selector: 'ui-builder-component',
+  templateUrl: './ui-builder.component.html'
+})
+
+export class UIBuilderComponent {
+  public primaryColor: string;
+  public secondaryColor: string;
+  public backgroundColor: string;
+  private text: string;
+  private optInText: string;
+  private thankYouText: string;
+
+ @ViewChild('staticTabs') staticTabs: TabsetComponent;
+
+  constructor(private router: Router,
+              private cpService: ColorPickerService) {}
+
+Next(){
+  console.log(this.primaryColor)
+  console.log(this.secondaryColor)
+  console.log(this.backgroundColor)
+  this.staticTabs.tabs[1].active = true;
+}
+
+Finish(){
+  console.log(this.text);
+  console.log(this.optInText);
+  console.log(this.thankYouText)
+}
+
+}
