7387aadb49517b588a0934e69c02c872f9ced578
leshikjanz <alex.tereshko@softeq.com>
2017-05-18 15:14:11 +0300

    Feat: made multi select for scroll

--- src/app/components/scroll/scroll.component.ts
+++ src/app/components/scroll/scroll.component.ts
@@ -9,7 +9,8 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 export class ScrollComponent implements OnInit {
   @Input() public options: string[];
 
-  @Output() public chose: EventEmitter<string> = new EventEmitter<string>();
+  @Output() public chose: EventEmitter<string[]> = new EventEmitter();
+  public selectedItems: string[] = [];
 
   public ngOnInit() {
     if (this.options == null) {
@@ -17,8 +18,25 @@ export class ScrollComponent implements OnInit {
     }
   }
 
+  /**
+   * Method choose uniq array elements
+   *
+   * @return {string[]}
+   * */
+  public unique(arr: string[]) {
+    const obj = {};
+
+      arr.forEach((a, i) => {
+        const str = a[i];
+        obj[str] = true;
+      });
+
+    return Object.keys(obj);
+  }
+
   public onChose(elem: string) {
-    console.log('onChose');
-    this.chose.emit(elem)
+    console.log(elem);
+    this.selectedItems.push(elem);
+    this.chose.emit(this.selectedItems);
   }
 }
