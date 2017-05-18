fafc54e28ad173185c8b85206e384b87a00b964c
Matthew Lee <matthew@getfoxtales.com>
2017-05-18 09:43:44 -0700

    bringing in correct event to create experience for

--- src/app/experience-builder/experience-builder-intro/experience-builder-intro.ts
+++ src/app/experience-builder/experience-builder-intro/experience-builder-intro.ts
@@ -1,11 +1,44 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { ExperienceBuilderService } from '../experience-builder.service';
 
 @Component({
   selector: 'experience-builder-intro',
   templateUrl: './experience-builder-intro.html'
 })
 
-export class ExperienceBuilderIntroComponent {
-  constructor(private router: Router) {}
+export class ExperienceBuilderIntroComponent implements OnInit {
+  public experienceName: string;
+  public selectedValue: string;
+
+   /**
+    * getting query parameter to create experience
+    *
+    */
+   public id: number;
+    public sub: any;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private experienceBuilderService: ExperienceBuilderService) {
+
+              }
+
+   public ngOnInit(){
+      this.sub = this.route.queryParams.subscribe(params =>{
+        this.id = +params['id'];
+      })
+
+
+   }
+ 
+
+  Next(){
+    this.experienceBuilderService.addExperience().subscribe((experience) => {
+     console.log(experience)
+   })
+    
+
+  }
 }
