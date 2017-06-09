3cad89dd085a0413f3cdc7f869c9f8c5d7fb42a5
leshikjanz <alex.tereshko@softeq.com>
2017-06-09 17:54:25 +0300

    Fix: fixed warnings

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
-import { RouteData } from "../shared/core/routing/route-data.service";
+import { Component } from '@angular/core';
+import { RouteData } from '../shared/core/routing/route-data.service';
 
 @Component({
   selector: 'admin',
@@ -8,6 +7,7 @@ import { RouteData } from "../shared/core/routing/route-data.service";
 })
 
 export class AdminComponent {
-  constructor(private _routeData:RouteData){
+  constructor(private _routeData: RouteData) {
     _routeData.name.next('Admin Management');
-  }}
+  }
+}
--- src/app/experience/list/experience-list.component.ts
+++ src/app/experience/list/experience-list.component.ts
@@ -12,28 +12,22 @@ import { ITableAction } from '../../shared/table/action.interface';
   styleUrls: ['./experience-list.component.scss']
 })
 export class ExperienceListComponent implements OnInit {
-   /**
-    * Experience
-    *
-    * @type {Experience[]}
-    */
-    public Experience: any[];
+  /**
+   * Experience
+   *
+   * @type {Experience[]}
+   */
+  public Experience: any[];
 
-   /**
-    * getting query parameter to find specific experiences
-    *
-    */
-    public id: number;
+  /**
+   * getting query parameter to find specific experiences
+   *
+   */
+  public id: number;
 
-    public status: any;
+  public status: any;
 
-    private sub: any;
-
-    constructor(
-      private experienceService: ExperienceService,
-      private route: ActivatedRoute) {
-    }
-
+  private sub: any;
 
   /**
    *  Experience actions
@@ -41,14 +35,18 @@ export class ExperienceListComponent implements OnInit {
    * @type {ITableAction[]}
    */
   public experienceActions: ITableAction[] = [
-    { id: 1, title: 'EDIT', callback: 'onEdit' },
-    { id: 2, title: 'CLONE', callback: 'onClone' },
-    { id: 3, title: 'ARCHIEVE', callback: 'onArchieve' },
-    { id: 4, title: 'ADD TO GROUP', callback: 'onAddToGroup' },
-    { id: 5, title: 'ASSIGN USERS', callback: 'onAssignUsers' },
+    {id: 1, title: 'EDIT', callback: 'onEdit'},
+    {id: 2, title: 'CLONE', callback: 'onClone'},
+    {id: 3, title: 'ARCHIEVE', callback: 'onArchieve'},
+    {id: 4, title: 'ADD TO GROUP', callback: 'onAddToGroup'},
+    {id: 5, title: 'ASSIGN USERS', callback: 'onAssignUsers'},
     {id: 6, title: 'RECAP REPORT', callback: 'onRecapReport'},
     {id: 7, title: 'EXPORT CRM DATA', callback: 'onExportCrmData'}
   ];
+
+  constructor(private experienceService: ExperienceService,
+              private route: ActivatedRoute) {
+  }
 
   /**
    * Search handler
@@ -60,26 +58,36 @@ export class ExperienceListComponent implements OnInit {
     console.log('onSearchChange');
   }
 
-    public ngOnInit(): void {
-      this.sub = this.route.params.subscribe((params) => {
-        this.id = params['id'];
-      });
-      console.log('this.id');
-      console.log(this.id);
+  /**
+   * Sort handler
+   *
+   * @param {string} value
+   * @returns {void}
+   */
+  public onSortChanged(value: string): void {
+    console.log('onSortChanged');
+  }
 
-      this.getExperiences();
-    }
-
-   /**
-    * Get experiences
-    *
-    * @returns {void}
-    */
-    public getExperiences(): void {
-        this.experienceService
-        .getExperiences(this.id)
-        .subscribe((experiences) => {
-          this.Experience = experiences;
+  public ngOnInit(): void {
+    this.sub = this.route.params.subscribe((params) => {
+      this.id = params['id'];
     });
+    console.log('this.id');
+    console.log(this.id);
+
+    this.getExperiences();
+  }
+
+  /**
+   * Get experiences
+   *
+   * @returns {void}
+   */
+  public getExperiences(): void {
+    this.experienceService
+      .getExperiences(this.id)
+      .subscribe((experiences) => {
+        this.Experience = experiences;
+      });
   }
 }
