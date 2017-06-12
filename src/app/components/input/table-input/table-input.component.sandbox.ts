780e1b7d47b8c801269c5a95991e2ba90eede57b
leshikjanz <alex.tereshko@softeq.com>
2017-06-13 00:48:23 +0300

    Feat: implemented animation from hover.css for back navigation component

--- src/app/components/input/table-input/table-input.component.sandbox.ts
+++ src/app/components/input/table-input/table-input.component.sandbox.ts
@@ -4,8 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 export default sandboxOf(TableInputComponent, { imports: [FormsModule] })
   .add('table-input', {
-    template: `<fox-input style="width: 300px; height: 65px;"
-              [title]="'title'" (Change)="value=$event"></fox-input>
+    template: `<table-input style="position: absolute; top: 100px; width: 300px; height: 65px;"
+              [title]="'title'" (Change)="value=$event"></table-input>
                You wrote: {{ value }}`,
     context: {
       title: 'Table input',
