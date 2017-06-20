cacab44c75c3285e155a97e5a8a33a8e638dc27b
Matthew Lee <matthew@getfoxtales.com>
2017-06-20 13:50:42 -0700

    fixing orientation selector

--- src/app/components/toggles/orientation-selector/orientation-selector.component.ts
+++ src/app/components/toggles/orientation-selector/orientation-selector.component.ts
@@ -7,14 +7,13 @@ import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
 })
 
 export class OrientationSelectorComponent {
-  @Input() public type: string = 'Bacon';
+  @Input() public type: string;
 
   @Input() public options: string[];
 
   @Output() public toggle: EventEmitter<string> = new EventEmitter<string>();
 
   public onChange(value: string) {
-      console.log(value)
     this.toggle.emit(value);
   }
 }
