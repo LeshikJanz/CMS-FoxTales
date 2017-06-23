f12fe7f1105c69e2a56ee53d249989b3f74145bd
Matthew Lee <matthew@getfoxtales.com>
2017-06-23 16:15:02 -0700

    fixed experience builder navigation to move along with the builder itself

--- src/app/components/navigations/experience-builder-navigation/experience-builder-navigation.component.ts
+++ src/app/components/navigations/experience-builder-navigation/experience-builder-navigation.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'experience-builder-navigation',
@@ -6,4 +7,93 @@ import { Component } from '@angular/core';
   styleUrls: ['experience-builder-navigation.component.scss']
 })
 
-export class ExperienceBuilderNavigationComponent {}
+export class ExperienceBuilderNavigationComponent implements OnInit, DoCheck {
+  public location: any = '';
+  public one: any;
+  public two: any;
+  public three: any;
+  public four: any;
+  public five: any;
+  public six: any;
+  public seven: any;
+  constructor(private router: Router){
+ 
+  }
+
+  public hello(){
+    return true;
+  }
+
+  ngOnInit(){
+    switch(this.router.url) {
+      case '/experience-builder/container/basic-details':
+        this.one = ' ';
+        this.two = this.three = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/content-options':
+        this.two = ' ';
+        this.one = this.three = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/sharing-options':
+        this.three = ' ';
+        this.one = this.two = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/email-builder':
+        this.four = ' ';
+        this.one = this.three = this.two = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/ui-options':
+        this.five = ' ';
+        this.one = this.three = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/content-gallery':
+        this.six = ' ';
+        this.one = this.three = this.four = this.two = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/content-feeds':
+        this.seven = ' ';
+        this.one = this.three = this.four = this.five = this.six = this.two = null;
+        break;
+      default:
+        this.one = ' ';
+        this.two = this.three = this.four = this.five = this.six = this.seven = null;
+    }
+  }
+
+  ngDoCheck(){
+    switch(this.router.url) {
+      case '/experience-builder/container/basic-details':
+        this.one = ' ';
+        this.two = this.three = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/content-options':
+        this.two = ' ';
+        this.one = this.three = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/sharing-options':
+        this.three = ' ';
+        this.one = this.two = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/email-builder':
+        this.four = ' ';
+        this.one = this.three = this.two = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/ui-options':
+        this.five = ' ';
+        this.one = this.three = this.four = this.five = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/content-gallery':
+        this.six = ' ';
+        this.one = this.three = this.four = this.two = this.six = this.seven = null;
+        break;
+      case '/experience-builder/container/content-feeds':
+        this.seven = ' ';
+        this.one = this.three = this.four = this.five = this.six = this.two = null;
+        break;
+      default:
+        this.one = ' ';
+        this.two = this.three = this.four = this.five = this.six = this.seven = null;
+    }
+  }
+
+}
