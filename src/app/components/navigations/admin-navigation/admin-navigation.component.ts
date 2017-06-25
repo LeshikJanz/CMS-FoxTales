a7663cffa9db8a4b99cbce25c3dcf14a862ead96
leshikjanz <alex.tereshko@softeq.com>
2017-06-25 19:32:23 +0300

    Feat: added archive/unarchive buttons to client navigation bar

--- src/app/components/navigations/admin-navigation/admin-navigation.component.ts
+++ src/app/components/navigations/admin-navigation/admin-navigation.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
+import { RouteData } from '../../../shared/core/event-management/route-data.service';
+import { IActionState } from '../../../client/client.interface';
 
 @Component({
   selector: 'admin-navigation',
@@ -11,10 +13,30 @@ import { Router } from '@angular/router';
 export class AdminNavigationComponent {
   public curLocation: string;
 
+  public isArchieved: boolean = false;
+
+  /**
+   * Archieve states
+   *
+   * @type {IActionState[]}
+   */
+  public clientStates: IActionState[] = [
+    {id: 1, action: 'Unarchived'},
+    {id: 2, action: 'Archived'}
+  ];
+
   constructor(private location: Location,
-              private router: Router) {
+              private router: Router,
+              private routeData: RouteData) {
     this.router.events.subscribe((val) =>
       this.curLocation = this.location.path()
     );
+  }
+
+  public onTypeChanged(id: number) {
+    this.isArchieved = !this.isArchieved;
+    console.log('id');
+    console.log(id);
+    this.routeData.archieve.next(id);
   }
 }
