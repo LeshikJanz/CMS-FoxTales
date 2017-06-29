36a63cb97937be1ff05419e37ebb144af47271b7
leshikjanz <alex.tereshko@softeq.com>
2017-06-29 08:30:17 +0300

    Feat: refactored user create/edit pages according to the new form-element

--- src/app/components/dropdowns/fox-select/fox-select.component.ts
+++ src/app/components/dropdowns/fox-select/fox-select.component.ts
@@ -23,27 +23,8 @@ export class FoxSelectComponent implements OnChanges {
 
   private value: any = {};
 
-  private _disabledV: string = '0';
-
-  private get disabledV(): string {
-    return this._disabledV;
-  }
-
-  private set disabledV(value: string) {
-    this._disabledV = value;
-    this.disabled = this._disabledV === '1';
-  }
-
   public selected(value: any): void {
     this.change.emit(value);
-  }
-
-  public removed(value: any): void {
-    console.log('Removed value is: ', value);
-  }
-
-  public typed(value: any): void {
-    console.log('New search input: ', value);
   }
 
   public refreshValue(value: any): void {
