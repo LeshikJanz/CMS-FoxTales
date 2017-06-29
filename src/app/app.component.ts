a8bc690a78d8f188c1341abba9d4cc38705abd27
Matthew Lee <matthew@getfoxtales.com>
2017-06-28 17:12:17 -0700

    app update to 2.2.14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
    *
    * @type {string}
    */
-  public version: string = '2.2.13';
+  public version: string = '2.2.14';
 
   /**
    * Route name
